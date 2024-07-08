2cff15297d765c1e3d2e1f26ff2a651f0e48b8c7
chintunikith <34469610+chintunikith@users.noreply.github.com>
2024-07-09 03:35:08 +0530

    Add files via upload

---
+++ DAKSH/JS/work.js
@@ -0,0 +1,48 @@
+    alert("Welcome To Daksh");
+
+function clearErrors() {
+
+    errors = document.getElementsByClassName('formerror');
+    for (let item of errors) {
+        item.innerHTML = "";
+    }
+
+
+}
+function seterror(id, error) {
+    element = document.getElementById(id);
+    element.getElementsByClassName('formerror')[0].innerHTML = error;
+
+}
+
+function validateForm() {
+    var returnval = true;
+    clearErrors();
+
+    var name = document.forms['myForm']["fname"].value;
+    if (name.length < 5) {
+        seterror("name", "*Length of name is too short");
+        returnval = false;
+    }
+
+    if (name.length == 0) {
+        seterror("name", "*Length of name cannot be zero!");
+        returnval = false;
+    }
+
+
+
+    var phone = document.forms['myForm']["fphone"].value;
+    if (phone.length != 10) {
+        seterror("phone", "*Phone number should be of 10 digits!");
+        returnval = false;
+    }
+    var Regno = document.forms['myForm']["fRegno"].value;
+    if (Regno.length != 9) {
+        seterror("Regno", "*Registration Number should be of 9 digits!");
+        returnval = false;
+    }
+
+
+    return returnval;
+}
